0b6fc1bcc1ab7bb91e62a6f6a4015d6d9c17338b
Pavel Astukov <p7ll7p@gmail.com>
2021-04-22 20:44:58 +0400

    added the repeatebility section

--- vpv1/vpv1.cpp
+++ vpv1/vpv1.cpp
@@ -1,5 +1,4 @@
-﻿//
-#include <time.h>
+﻿#include <time.h>
 #include <iostream>
 #include <intrin.h>
 #include <windows.h>
@@ -7,12 +6,14 @@
 #include <math.h>
 #include <functional>
 #include <vector>
+#include <fstream>
+
 
 using namespace std;
 
 // n & 2 => x1 because 1(10) = 000010(2) 
 bool myFunction(unsigned short n) {
-	cout << "x1x2x3 | x2!x3x4 | x1!x2 | x1!x2x3!x4 | x3x4";
+	cout << "x1x2x3 | x2!x3x4 | x1!x2 | x1!x2x3!x4 | x3x4\n";
 	//             x1                x2                x3                 
 	return ((((n & 2) >> 1) && ((n & 4) >> 2) && ((n & 8) >> 3))
 		//       |        x2                !x3                x4            
@@ -105,6 +106,7 @@ long measureByQPC(int n, std::function<long(unsigned __int64)> func) {
 		res = func(n);
 	}
 	QueryPerformanceCounter(&t_finish);
+	cout << "частота:" << freqQPC.QuadPart << endl;
 	auto deltaQPC = t_finish.QuadPart - t_start.QuadPart;
 	double delta = (deltaQPC * 1.0) / counter;
 	delta = delta / freqQPC.QuadPart;
@@ -135,7 +137,7 @@ double statistic(vector<int>& arr, long long n, double& average, double& msd) {
 void generate(vector<int>& arr, int n) {
 	for (size_t i = 0; i < n; i++)
 	{
-		arr[i] = i * 10 + 1;
+		arr[i] = i + 1;
 	}
 }
 struct RP
@@ -161,7 +163,7 @@ void clockRP(vector<int>& vec) {
 		rp.n = n;
 		rp.time = time;
 		results.push_back(rp);
-		n += 10;
+		n += 1;
 		if (results.size() > 1 && results[results.size() - 1].time > 0 && results[results.size() - 2].time > 0) {
 			cout << "n-1 = " << results[results.size() - 2].n << " time = " << results[results.size() - 2].time << " с";
 			cout << endl;
@@ -182,46 +184,158 @@ void tscRP() {
 	end = __rdtsc();
 	unsigned long long delta = end - start;
 	double time = (delta * 1.0) / frequency;
-	time *= 1e9;
-	cout << "для TSC\n" << time << " нс\n";
+	//time *= 1e9;
+	cout << "для TSC\n" << time << " с\n";
 }
 
+double increamentVector(vector<int>& vec, int n) {
+	for (size_t i = 0; i < n; i++)
+	{
+		vec[i]++;
+	}
+	return vec[n - 1] * 1.0;
+}
 void QPCRP(vector<int>& vec) {
 	LARGE_INTEGER t_start, t_finish, freqQPC;
-	QueryPerformanceFrequency(&freqQPC); // получаем частоту
 	vector<RP> results;
-	long long n = 100;
+	long long n = 1;
 	cout << "для QPC\n";
 	while (n < vec.size()) {
 		double average = 0.0;
 		double msd = 0.0;
 		QueryPerformanceCounter(&t_start); // засекаем время старта CODE
-		average = statistic(vec, n, average, msd);
+		//average = statistic(vec, n, average, msd);
+		//average = increamentVector(vec, n);
 		QueryPerformanceCounter(&t_finish);
+		QueryPerformanceFrequency(&freqQPC); // получаем частоту
 		auto deltaQPC = t_finish.QuadPart - t_start.QuadPart;
-		double time = (deltaQPC * 1.0) / freqQPC.QuadPart;
-		time *= 1e9;
+		cout << "частота:" << freqQPC.QuadPart << endl;
+		double delta = (deltaQPC * 1.0);
+		delta = delta / freqQPC.QuadPart;
+		//delta *= 1e9;
+		double time = delta;
+
 
 		cout.precision(5);
 		RP rp;
 		rp.n = n;
 		rp.time = time;
 		results.push_back(rp);
-		n += 10;
+		n += 1;
 		if (results.size() > 1 && results[results.size() - 1].time > 0 && results[results.size() - 2].time > 0) {
-			cout << "n-1 = " << results[results.size() - 2].n << " time = " << results[results.size() - 2].time << " нс";
+			cout << "n-1 = " << results[results.size() - 2].n << " time = " << results[results.size() - 2].time << " с";
 			cout << endl;
-			cout << "n   = " << results[results.size() - 1].n << " time = " << results[results.size() - 1].time << " нс";
+			cout << "n   = " << results[results.size() - 1].n << " time = " << results[results.size() - 1].time << " с";
 			cout << endl;
 
 			return;
 		}
 	}
 }
+string path = "E:\\programProjects\\C++\\vpv1\\";
+void generateRandom4bitNum() {
+	ofstream out;
+	out.open(path + "randoms.txt");
+	for (size_t i = 0; i < 10000; i++)
+	{
+		out << rand() % 16 << endl;
+	}
+	out.close();
+}
+
+void read4bitNum(vector<int>& vec) {
+	ifstream in;
+	in.open(path + "randoms.txt");
+
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		in >> vec[i];
+	}
+}
+
+//void writeTimes2File() {
+//	ofstream out;
+//	out.open(path + "repeatabilityByClock");
+//
+//}
+void repeatabilityClock() {
+	vector<double> results;
+	for (size_t i = 0; i < 1000; i++)
+	{
+		vector<int> rand4bitNum(10000);
+		read4bitNum(rand4bitNum);
+		clock_t start = clock();
+		for (size_t i = 0; i < rand4bitNum.size(); i++)
+		{
+			bool res = myFunction(15);
+		}
+		clock_t end = clock();
+		clock_t delta = end - start;
+		double time = (delta * 1.0) / CLOCKS_PER_SEC;
+		time /= rand4bitNum.size();
+		//time *= 1e0;
+		results.push_back(time);
+	}
+
+
+
+	cout.precision(5);
+	cout << "повторяемость время " << time << endl;
+}
+
+void repeatabilityTSC() {
+	vector<double> results;
+	for (size_t i = 0; i < 1000; i++)
+	{
+		vector<int> rand4bitNum(10000);
+		read4bitNum(rand4bitNum);
+
+		unsigned long long start;
+		unsigned long long end;
+		unsigned long long frequency = getFrequencyForTSC();
+		start = __rdtsc();
+		for (size_t i = 0; i < rand4bitNum.size(); i++)
+		{
+			bool res = myFunction();
+		}
+		end = __rdtsc();
+		unsigned long long deltaTSC = end - start;
+		double time = (deltaTSC * 1.0) / frequency;
+		time *= 1e9;
+		results.push_back(time);
+
+	}
+}
+
+void repeatabilityQPC() {
+	vector<double> results;
+	for (size_t i = 0; i < 1000; i++)
+	{
+		LARGE_INTEGER t_start, t_finish, freqQPC;
+		QueryPerformanceFrequency(&freqQPC); // получаем частоту
+		for (size_t i = 0; i < length; i++)
+		{
+
+		}
+		QueryPerformanceCounter(&t_start); // засекаем время старта CODE
+		bool res = myFunction(randVar);
+
+		QueryPerformanceCounter(&t_finish);
+		auto deltaQPC = t_finish.QuadPart - t_start.QuadPart;
+		double time = (deltaQPC * 1.0) / freqQPC.QuadPart;
+		time *= 1e9;
+
+		results.push_back(time);
+
+	}
+}
 int main()
 {
+	//generateRandom4bitNum();
+	//vector<int> rand4bitNum(10000);
+	//read4bitNum(rand4bitNum);
 	setlocale(LC_ALL, "Russian");
-	cout << "menu: 0 - сделать все, 1 - фибоначчи, 2 - разрешающая способность\n";
+	cout << "menu: 0 - сделать все, 1 - фибоначчи, 2 - разрешающая способность, 3 - повторяемость результатов\n";
 	int menu;
 	cin >> menu;
 	if (menu == 1 || menu == 0) {
@@ -248,6 +362,9 @@ int main()
 		tscRP();
 		QPCRP(vec);
 
+	}
+	else if (menu == 3 || menu == 0) {
+		repeatabilityClock();
 	}
 
 
